52998007fb60bc1752735ecf7424fdc425fa79e4
klgd <slice1213@gmail.com>
2017-10-19 09:20:43 +0800

    update

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Category::class, function (Faker $faker) {
     return [
+        'parent_id' => 0,
         'name' => $faker->word,
         'slug' => $faker->slug(1),
     ];
