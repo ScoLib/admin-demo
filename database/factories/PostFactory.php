1f1e35c174484c10bdd4a3a4b0d331ff6be26730
klgd <slice1213@gmail.com>
2017-12-20 17:46:53 +0800

    update

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,8 +5,10 @@ use Faker\Generator as Faker;
 $factory->define(\App\Post::class, function (Faker $faker) {
     $category = \App\Category::first();
     return [
-        'title' => $faker->sentence,
-        'content' => $faker->text,
-        'category_id' => $category->id,
+        'title'        => $faker->sentence,
+        'content'      => $faker->text,
+        'category_id'  => $category->id,
+        'is_excellent' => rand(0, 1),
+        'published'    => rand(0, 1),
     ];
 });
