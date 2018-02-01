d044db008012e92d260a6ebef2c71716966b46a3
klgd <slice1213@gmail.com>
2018-02-01 17:56:48 +0800

    update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
             $role = (new $roleModelName())->where('name', 'admin')->firstOrFail();
             $user->name = 'admin';
             $user->email = 'admin@admin.com';
-            $user->password = '123456';
+            $user->password = bcrypt('123456');
             $user->save();
             $user->attachRole($role);
         }
